25518f619b66c2ac308bdc1842e70222a4f831c0
m_juhdi <mjfahriza@gmail.com>
2026-01-30 09:16:26 +0700

    nambah link

--- mjuhdi/biodata/navbar.php
+++ mjuhdi/biodata/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg" style="background-color:#000C8A" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="http://localhost/mjuhdi/biodata/">Navbar</a>
+            <a class="navbar-brand" href="http://localhost/coding.php/mjuhdi/biodata/">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="http://localhost/mjuhdi/biodata/siswa/">Siswa</a>
+                        <a class="nav-link active" aria-current="page" href="http://localhost/coding.php/mjuhdi/biodata/siswa/">Siswa</a>
                     </li>
                     
                 </ul>
